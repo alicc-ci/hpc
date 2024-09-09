3d270a1014643f05915a4ec7e4307ebafd903b7f
wxy <1102655625@qq.com>
2024-09-09 16:15:08 +0800

    忽略  文件夹

--- lab2-naive-gemm/link/main.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int sum(int n);
-int main(){
-    printf("%d\n", sum(100));
-}
--- lab2-naive-gemm/link/sum.c
+++
@@ -1,7 +0,0 @@
-int sum(int n){
-    int sum=0;
-    for(int i=1; i<=n; i++){
-        sum += i;
-    }
-    return sum;
-}
