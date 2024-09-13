11ab4d160e64c4f37a052cd218874432aeefeab3
wxy <1102655625@qq.com>
2024-09-13 16:51:27 +0800

    lab5初步写写

--- my_code/how-to-optimize-gemm/test_MMult.c
+++ my_code/how-to-optimize-gemm/test_MMult.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <cblas.h>
 #include "defs.h"
+#include <sys/time.h>
+#include <pthread.h>
+#include "MY_MMult.c"
+
+#define NUM_THREADS 4  // 定义线程数量
+
+typedef struct {
+    double *A;
+    double *B;
+    double *C;
+    int m, n, k;
+    int start_row;
+    int end_row;
+} thread_data_t;
 
 void REF_MMult(int, int, int, double *, int, double *, int, double *, int);
 void MY_MMult(int, int, int, double *, int, double *, int, double *, int);
@@ -86,6 +100,21 @@ int main()
         dtime_best = (dtime < dtime_best ? dtime : dtime_best);
     }
 
+    for (rep = 0; rep < NREPEATS; rep++)
+    {
+      copy_matrix(m, n, cold, ldc, c, ldc);
+
+      /* Time your implementation */
+      dtime = dclock();
+      MY_MMult_pthread(m, n, k, a, lda, b, ldb, c, ldc);
+      dtime = dclock() - dtime;
+
+      if (rep == 0)
+        dtime_best = dtime;
+      else
+        dtime_best = (dtime < dtime_best ? dtime : dtime_best);
+    }
+
     diff = compare_matrices(m, n, c, ldc, cref, ldc);
 
     printf("%d %le %le \n", p, gflops / dtime_best, diff);
@@ -101,4 +130,67 @@ int main()
   printf("];\n");
 
   exit(0);
+}
+
+// 多线程DGEMM计算部分
+void *thread_dgemm(void *arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    double *A = data->A;
+    double *B = data->B;
+    double *C = data->C;
+    int m = data->m;
+    int n = data->n;
+    int k = data->k;
+    int start_row = data->start_row;
+    int end_row = data->end_row;
+
+    for (int i = start_row; i < end_row; i++) {
+        for (int j = 0; j < n; j++) {
+            double sum = 0.0;
+            for (int p = 0; p < k; p++) {
+                sum += A[i * k + p] * B[p * n + j];
+            }
+            C[i * n + j] = sum;
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+void MY_MMult_pthread(int m, int n, int k, double *A, int lda, double *B, int ldb, double *C, int ldc) {
+    pthread_t threads[NUM_THREADS];
+    thread_data_t thread_data[NUM_THREADS];
+    int rows_per_thread = m / NUM_THREADS;
+
+    // 记录起始时间
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    // 分配每个线程负责的行
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data[i].A = A;
+        thread_data[i].B = B;
+        thread_data[i].C = C;
+        thread_data[i].m = m;
+        thread_data[i].n = n;
+        thread_data[i].k = k;
+        thread_data[i].start_row = i * rows_per_thread;
+        thread_data[i].end_row = (i == NUM_THREADS - 1) ? m : (i + 1) * rows_per_thread;
+
+        pthread_create(&threads[i], NULL, thread_dgemm, (void *)&thread_data[i]);
+    }
+
+    // 等待所有线程完成
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // 记录结束时间
+    gettimeofday(&end, NULL);
+
+    // 计算时间差
+    double time_taken = (end.tv_sec - start.tv_sec) * 1e6;
+    time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
+
+    // printf("MY_MMult (多线程) 完成，耗时: %f 秒\n", time_taken);
 }
